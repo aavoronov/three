83d227c2df1b4a3b918f29e03d9e49c3f1c3a4a2
Andrey Voronov <avoronov@syntellect.ru>
2024-02-24 19:55:51 +0300

    layout fixes

--- src/ui/boardModel.tsx
+++ src/ui/boardModel.tsx
@@ -161,9 +161,7 @@ const BoardModel = observer(({ viewModel }: Props) => {
           color={vm.lightningsParams.color}
         />
       )}
-      <div
-        className='controlPanel'
-        style={vm.menuIsOpen ? { display: "flex", zIndex: 10, width: 300, transition: "all 0.2s", height: "90vh" } : undefined}>
+      <div className={`controlPanel${vm.menuIsOpen ? " opened" : ""}`}>
         <div className='boardSizeBtns'>
           <button className='boardSizeBtn' onClick={vm.shrinkBoard}>
             -
@@ -215,146 +213,150 @@ const BoardModel = observer(({ viewModel }: Props) => {
         <button onClick={vm.toggledebugMode}>{!vm.debugMode ? "Режим отладки" : "Обычный режим"}</button>
         <span style={{ display: "none" }}>Ага, думаешь, так просто? Введи пароль . Попался! Ищешь пароль? Страждущий да обрящет</span>
       </div>
-      <div className='stats'>
-        <span className='gamemode'>{vm.modeText}</span>
+      <div className='game'>
+        <div className='stats'>
+          <span className='stats-line'>{vm.modeText}</span>
 
-        {(() => {
-          if (!vm.multiplayerText) return;
-          return (
-            <span className='gamemode'>
-              {vm.multiplayerText.startText}
-              <span style={{ color: vm.multiplayerText.currentColor.code }}>{vm.multiplayerText.currentColor.name}. </span>
-              <span className={`movesLeft ${vm.extraMoveAwarded ? "extraMoveAwarded" : ""}`}>{vm.multiplayerText.endText}</span>
-            </span>
-          );
-        })()}
-
-        {/* <button onClick={() => vm.shuffleBoard()}>extra move</button> */}
-        {/* <button onClick={() => console.log(bot.current)}>test</button> */}
-
-        {vm.movesMade ? (
-          <>
-            <span style={{ fontSize: 18 }}>{vm.time}</span>
-
-            {vm.constraintGamemode !== constraintGamemodes.multiplayer && vm.constraintGamemode !== constraintGamemodes.bot && (
-              <span>{vm.singleplayerScore}</span>
-            )}
-            {vm.constraintGamemode === constraintGamemodes.multiplayer || vm.constraintGamemode === constraintGamemodes.bot ? (
-              <div className='twoPlayerStatsWrap'>
-                <div className='twoPlayersStats'>
-                  <div className='playerWrap'>
-                    <span style={{ color: vm.multiplayerScore.blue.color }}>{vm.multiplayerScore.blue.score}</span>
-                    <div className='perks blue'>
-                      <span
-                        className={vm.calculatePerksClassNames(perks.shuffle, 1)}
-                        onClick={() => vm.usePerk(perks.shuffle, "blue")}></span>
-                      <span className={vm.calculatePerksClassNames(perks.bomb, 1)} onClick={() => vm.usePerk(perks.bomb, "blue")}></span>
-                      <span
-                        className={vm.calculatePerksClassNames(perks.hammer, 1)}
-                        onClick={() => vm.usePerk(perks.hammer, "blue")}></span>
-                    </div>
-                  </div>
-                  <div className='playerWrap'>
-                    <span style={{ color: vm.multiplayerScore.red.color }}>{vm.multiplayerScore.red.score}</span>
-                    <div className='perks red'>
-                      <span
-                        className={vm.calculatePerksClassNames(perks.shuffle, 2)}
-                        onClick={() => vm.usePerk(perks.shuffle, "red")}></span>
-                      <span className={vm.calculatePerksClassNames(perks.bomb, 2)} onClick={() => vm.usePerk(perks.bomb, "red")}></span>
-                      <span className={vm.calculatePerksClassNames(perks.hammer, 2)} onClick={() => vm.usePerk(perks.hammer, "red")}></span>
-                    </div>
-                  </div>
-                </div>
-
-                {vm.gameOver && <span style={{ color: vm.winner.color }}>{vm.winner.text}</span>}
-              </div>
-            ) : vm.constraintGamemode !== "moves" ? (
-              <span>
-                Ходов сделано: {vm.movesMade}
-                {vm.movesMade > 99 && ". Невероятно!"}
+          {(() => {
+            if (!vm.multiplayerText) return;
+            return (
+              <span className='stats-line'>
+                {vm.multiplayerText.startText}
+                <span style={{ color: vm.multiplayerText.currentColor.code }}>{vm.multiplayerText.currentColor.name}. </span>
+                <span className={`movesLeft ${vm.extraMoveAwarded ? "extraMoveAwarded" : ""}`}>{vm.multiplayerText.endText}</span>
               </span>
-            ) : !vm.gameOver ? (
-              <span>Ходов осталось: {vm.movesLeft}</span>
-            ) : (
-              <span>Ходы закончились!</span>
-            )}
-          </>
-        ) : null}
-        {vm.gameOver && <button onClick={vm.handleReplay}>Переиграть</button>}
-      </div>
+            );
+          })()}
 
-      <div
-        className={`board ${vm.hammerMode ? "hammerMode" : ""} ${!vm.boardStabilized ? "locked" : ""}`}
-        // ref={board}
-        onClick={(event: React.SyntheticEvent<HTMLSpanElement, MouseEvent>) => {
-          vm.breakPieceInHammerMode(event);
-          if (!vm.debugMode) return;
+          {/* <button onClick={() => vm.shuffleBoard()}>extra move</button> */}
+          {/* <button onClick={() => console.log(bot.current)}>test</button> */}
 
-          const index = parseInt((event.target as HTMLSpanElement).attributes["data-key"].value);
-          console.log(index);
-          // vm.testFn(index);
-          // vm.explodeSpecials(index);
-          // console.log(vm.getPiecesColor(index));
-          // hammerMode && explodeSpecials([event.target.attributes["data-key"].value]) && setHammerMode(false);
-          // debugMode && console.log(currentPieces[event.target.attributes["data-key"].value].split(" ")[0]);
-        }}
-        style={{ gridTemplateColumns: `repeat(${vm.boardSize}, 1fr)`, height: vm.boardSize * 50, width: vm.boardSize * 50 }}>
-        {/* {!vm.boardStabilized ? " moveLocked" : ""} */}
-        {vm.currentPieces.map((e, i) => (
-          <span
-            className={"piece " + e}
-            key={i}
-            data-key={i}
-            draggable={vm.debugMode ? true : false}
-            onDragOver={(e) => {
-              e.preventDefault();
-            }}
-            onDragEnter={(e) => {
-              e.preventDefault();
-            }}
-            onDragLeave={(e) => {
-              e.preventDefault();
-            }}
-            onDrop={vm.dragDrop}
-            onDragStart={vm.dragStart}
-            onDragEnd={vm.dragEnd}
-            {...swipeHandlers}></span>
-        ))}
-      </div>
-      <div className='rules'>
-        <div className='moveRules'>
-          <div className='rule'>
-            <div className='arrowRule' style={{ marginRight: 10 }}></div>
-            <span style={{ color: "white" }}>=</span>
-            <span className='piece arrowHorizontal' style={{ width: 40, height: 40 }}></span>
-          </div>
-          <div className='rule'>
-            <div className='bombRule' style={{ marginRight: 10 }}></div>
-            <span style={{ color: "white" }}>=</span>
-            <span className='piece bomb' style={{ width: 40, height: 40 }}></span>
-          </div>
-          <div className='rule'>
-            <div className='lightningRule' style={{ marginRight: 10 }}></div>
-            <span style={{ color: "white" }}>=</span>
-            <span className='piece lightning' style={{ width: 40, height: 40 }}></span>
-          </div>
-          <div className='rule'>
-            <div className='doubleSpecialRule' style={{ marginRight: 10 }}></div>
-            <span style={{ color: "white" }}>=</span>
-            <div className='explosion' style={{ marginRight: 10 }}></div>
-          </div>
+          {vm.movesMade ? (
+            <>
+              <span className='stats-line'>{vm.time}</span>
+
+              {vm.constraintGamemode !== constraintGamemodes.multiplayer && vm.constraintGamemode !== constraintGamemodes.bot && (
+                <span className='stats-line'>{vm.singleplayerScore}</span>
+              )}
+              {vm.constraintGamemode === constraintGamemodes.multiplayer || vm.constraintGamemode === constraintGamemodes.bot ? (
+                <div className='twoPlayerStatsWrap'>
+                  <div className='twoPlayersStats'>
+                    <div className='playerWrap'>
+                      <span style={{ color: vm.multiplayerScore.blue.color }}>{vm.multiplayerScore.blue.score}</span>
+                      <div className='perks blue'>
+                        <span
+                          className={vm.calculatePerksClassNames(perks.shuffle, 1)}
+                          onClick={() => vm.usePerk(perks.shuffle, "blue")}></span>
+                        <span className={vm.calculatePerksClassNames(perks.bomb, 1)} onClick={() => vm.usePerk(perks.bomb, "blue")}></span>
+                        <span
+                          className={vm.calculatePerksClassNames(perks.hammer, 1)}
+                          onClick={() => vm.usePerk(perks.hammer, "blue")}></span>
+                      </div>
+                    </div>
+                    <div className='playerWrap'>
+                      <span style={{ color: vm.multiplayerScore.red.color }}>{vm.multiplayerScore.red.score}</span>
+                      <div className='perks red'>
+                        <span
+                          className={vm.calculatePerksClassNames(perks.shuffle, 2)}
+                          onClick={() => vm.usePerk(perks.shuffle, "red")}></span>
+                        <span className={vm.calculatePerksClassNames(perks.bomb, 2)} onClick={() => vm.usePerk(perks.bomb, "red")}></span>
+                        <span
+                          className={vm.calculatePerksClassNames(perks.hammer, 2)}
+                          onClick={() => vm.usePerk(perks.hammer, "red")}></span>
+                      </div>
+                    </div>
+                  </div>
+
+                  {vm.gameOver && <span style={{ color: vm.winner.color }}>{vm.winner.text}</span>}
+                </div>
+              ) : vm.constraintGamemode !== "moves" ? (
+                <span className='stats-line'>
+                  Ходов сделано: {vm.movesMade}
+                  {vm.movesMade > 99 && ". Невероятно!"}
+                </span>
+              ) : !vm.gameOver ? (
+                <span className='stats-line'>Ходов осталось: {vm.movesLeft}</span>
+              ) : (
+                <span className='stats-line'>Ходы закончились!</span>
+              )}
+            </>
+          ) : null}
+          {vm.gameOver && <button onClick={vm.handleReplay}>Переиграть</button>}
         </div>
 
-        {vm.differentValueMode && (
-          <div className='valueRules'>
-            {vm.classes.map((item, index) => (
-              <div className='rule' key={index}>
-                <span className={`piece ${item}`} style={{ width: 40, height: 40, marginRight: 10 }}></span>
-                <span style={{ position: "absolute", left: 17, color: "#29323c" }}>{index + 1}</span>
-              </div>
-            ))}
+        <div
+          className={`board${vm.hammerMode ? " hammerMode" : ""}${!vm.boardStabilized ? " locked" : ""}`}
+          // ref={board}
+          onClick={(event: React.SyntheticEvent<HTMLSpanElement, MouseEvent>) => {
+            vm.breakPieceInHammerMode(event);
+            if (!vm.debugMode) return;
+
+            const index = parseInt((event.target as HTMLSpanElement).attributes["data-key"].value);
+            console.log(index);
+            // vm.testFn(index);
+            // vm.explodeSpecials(index);
+            // console.log(vm.getPiecesColor(index));
+            // hammerMode && explodeSpecials([event.target.attributes["data-key"].value]) && setHammerMode(false);
+            // debugMode && console.log(currentPieces[event.target.attributes["data-key"].value].split(" ")[0]);
+          }}
+          style={{ gridTemplateColumns: `repeat(${vm.boardSize}, 1fr)`, height: vm.boardSize * 50, width: vm.boardSize * 50 }}>
+          {/* {!vm.boardStabilized ? " moveLocked" : ""} */}
+          {vm.currentPieces.map((e, i) => (
+            <span
+              className={"piece " + e}
+              key={i}
+              data-key={i}
+              draggable={vm.debugMode ? true : false}
+              onDragOver={(e) => {
+                e.preventDefault();
+              }}
+              onDragEnter={(e) => {
+                e.preventDefault();
+              }}
+              onDragLeave={(e) => {
+                e.preventDefault();
+              }}
+              onDrop={vm.dragDrop}
+              onDragStart={vm.dragStart}
+              onDragEnd={vm.dragEnd}
+              {...swipeHandlers}></span>
+          ))}
+        </div>
+        <div className='rules'>
+          <div className='moveRules'>
+            <div className='rule'>
+              <div className='arrowRule' style={{ marginRight: 10 }}></div>
+              <span style={{ color: "white" }}>=</span>
+              <span className='piece arrowHorizontal' style={{ width: 40, height: 40 }}></span>
+            </div>
+            <div className='rule'>
+              <div className='bombRule' style={{ marginRight: 10 }}></div>
+              <span style={{ color: "white" }}>=</span>
+              <span className='piece bomb' style={{ width: 40, height: 40 }}></span>
+            </div>
+            <div className='rule'>
+              <div className='lightningRule' style={{ marginRight: 10 }}></div>
+              <span style={{ color: "white" }}>=</span>
+              <span className='piece lightning' style={{ width: 40, height: 40 }}></span>
+            </div>
+            <div className='rule'>
+              <div className='doubleSpecialRule' style={{ marginRight: 10 }}></div>
+              <span style={{ color: "white" }}>=</span>
+              <div className='explosion' style={{ marginRight: 10 }}></div>
+            </div>
           </div>
-        )}
+
+          {vm.differentValueMode && (
+            <div className='valueRules'>
+              {vm.classes.map((item, index) => (
+                <div className='rule' key={index}>
+                  <span className={`piece ${item}`} style={{ width: 40, height: 40, marginRight: 10 }}></span>
+                  <span style={{ position: "absolute", left: 17, color: "#29323c" }}>{index + 1}</span>
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
